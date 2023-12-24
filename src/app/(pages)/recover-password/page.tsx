c4295a36fb1a7e5e26b60f9c533b5237c70be48f
Shafi Walsher <shafiwalsher@gmail.com>
2023-12-24 16:19:54 +0530

    final

---
+++ src/app/(pages)/recover-password/page.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Metadata } from 'next'
+
+import { Gutter } from '../../_components/Gutter'
+import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
+import { RecoverPasswordForm } from './RecoverPasswordForm'
+
+import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
+import { RenderParams } from '../../_components/RenderParams'
+
+export default async function RecoverPassword() {
+  return (
+    <section className={classes.recoverPassword}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image src="/logo-dark.svg" alt="logo" width={250} height={23} className={classes.logo} />
+        </Link>
+      </div>
+
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+          <Link href="/login" className={classes.backLink}>
+            <Image src="/assets/icons/arrow-left.svg" alt="leftArrow" width={24} height={24} />
+            <p>Back</p>
+          </Link>
+          <div className={classes.formTitle}>
+            <h3>Forgot Pasword?</h3>
+          </div>
+          <RecoverPasswordForm />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'Recover Password',
+  description: 'Enter your email address to recover your password.',
+  openGraph: mergeOpenGraph({
+    title: 'Recover Password',
+    url: '/recover-password',
+  }),
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Metadata } from 'next'
+import { Noto_Sans } from 'next/font/google'
+
+import { AdminBar } from './_components/AdminBar'
+import { Footer } from './_components/Footer'
+import { Header } from './_components/Header'
+import { Providers } from './_providers'
+import { InitTheme } from './_providers/Theme/InitTheme'
+import { mergeOpenGraph } from './_utilities/mergeOpenGraph'
+
+import './_css/app.scss'
+
+const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-noto',
+})
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <InitTheme />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
+      <body className={notoSans.variable}>
+        <Providers>
+          <AdminBar />
+          {/* @ts-expect-error */}
+          <Header />
+          <main className="main">{children}</main>
+          {/* @ts-expect-error */}
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  )
+}
+
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SERVER_URL || 'https://payloadcms.com'),
+  twitter: {
+    card: 'summary_large_image',
+    creator: '@payloadcms',
+  },
+  openGraph: mergeOpenGraph(),
+}
